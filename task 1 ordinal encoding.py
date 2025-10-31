27d03119613754b776660e8b5bc90fa56b95874b
aggarwalavni <aggarwalavni08@gmail.com>
2025-10-31 22:32:56 +0530

    Add files via upload

---
+++ task 1 ordinal encoding.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = {'Size': ['Small fries', 'Medium fries', 'Large fries', 'Extra large fries' 'Medium fries', 'Small fries']}
+df = pd.DataFrame(data)
+size_order = ['Small fries', 'Medium fries', 'Large fries','Extra large fries']
+
+encoded_val = []
+for val in df['Size']:
+    for i in range(len(size_order)):
+        if val == size_order[i]:
+            encoded_val.append(i)
+            break
+
+df['Encoded'] = encoded_val
+print("After Manual Ordinal Encoding Dataframe:")
+print(df)
