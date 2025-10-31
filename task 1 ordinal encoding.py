801dbb88a0b403f92a5641598bf45852555081d1
aggarwalavni <aggarwalavni08@gmail.com>
2025-10-31 22:40:53 +0530

    Update task 1 ordinal encoding.py

--- task 1 ordinal encoding.py
+++ task 1 ordinal encoding.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-data = {'Size': ['Small fries', 'Medium fries', 'Large fries', 'Extra large fries' 'Medium fries', 'Small fries']}
+data = {'Fries': ['Small fries', 'Medium fries', 'Large fries', 'Extra large fries' 'Medium fries', 'Small fries']}
 df = pd.DataFrame(data)
 size_order = ['Small fries', 'Medium fries', 'Large fries','Extra large fries']
 
 encoded_val = []
-for val in df['Size']:
+for val in df['Fries']:
     for i in range(len(size_order)):
         if val == size_order[i]:
             encoded_val.append(i)
@@ -14,3 +14,4 @@ for val in df['Size']:
 df['Encoded'] = encoded_val
 print("After Manual Ordinal Encoding Dataframe:")
 print(df)
+
